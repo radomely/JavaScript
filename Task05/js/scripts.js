de7c9e32ec5a09b84c14dcf1ed91b39bde45a1e4
Andrii <radomely@gmail.com>
2019-01-21 18:39:38 +0200

    added Task05

--- Task05/js/scripts.js
+++ Task05/js/scripts.js
@@ -110,62 +110,62 @@
 */
 // const findGreaterThan = (num, arr) => {
 //     const result = [];
-  
+
 //     for (let i = 0, max = arr.length; i < max; i += 1) {
 //       if (arr[i] > num) {
 //         result.push(arr[i]);
 //       }
 //     }
-  
+
 //     return result;
 //   };
-  
-  const findGreaterThan = (num, arr) => arr.filter(el=>el>num);
 
-  console.log( findGreaterThan(2, [1, 2, 3, 4, 5]) ); // [3, 4, 5,]
-  console.log( findGreaterThan(3, [1, 2, 3, 4, 5]) ); // [4, 5,]
-  console.log( findGreaterThan(1, [1, 2, 3, 4, 5]) ); // [2, 3, 4, 5,]
-  
-  /* 
+const findGreaterThan = (num, arr) => arr.filter(el => el > num);
+
+console.log(findGreaterThan(2, [1, 2, 3, 4, 5])); // [3, 4, 5,]
+console.log(findGreaterThan(3, [1, 2, 3, 4, 5])); // [4, 5,]
+console.log(findGreaterThan(1, [1, 2, 3, 4, 5])); // [2, 3, 4, 5,]
+
+/* 
     Функция multiplyBy принимает два аргумента - число и массив. 
     Возвращает массив все значения которого умножены на число.
   */
 //   const multiplyBy = (num, arr) => {
 //     let result = [];
-  
+
 //     for (let i = 0, max = arr.length; i < max; i += 1) {
 //       result.push(arr[i] * num);
 //     }
-  
+
 //     return result;
 //   };
-  
-  const multiplyBy = (num, arr) => arr.map(el=>el*num);
 
-  console.log( multiplyBy(2, [1, 2, 3, 4, 5]) ); // [2, 4, 6, 8, 10]
-  console.log( multiplyBy(3, [1, 2, 3, 4, 5]) ); // [3, 6, 9, 12, 15]
-  console.log( multiplyBy(4, [1, 2, 3, 4, 5]) ); // [4, 8, 12, 16, 20]
-  
-  /* 
+const multiplyBy = (num, arr) => arr.map(el => el * num);
+
+console.log(multiplyBy(2, [1, 2, 3, 4, 5])); // [2, 4, 6, 8, 10]
+console.log(multiplyBy(3, [1, 2, 3, 4, 5])); // [3, 6, 9, 12, 15]
+console.log(multiplyBy(4, [1, 2, 3, 4, 5])); // [4, 8, 12, 16, 20]
+
+/* 
     Функция summAllNumbers принимает любое число аргументов.
     Возвращает число - сумму всех аргументов.
   */
 //   function summAllNumbers(...args) {
 //     let accumulator = 0;
-  
+
 //     for (let i = 0, max = args.length; i < max; i += 1) {
 //       accumulator += args[i];
 //     }
-  
+
 //     return accumulator;
 //   }
-  const summAllNumbers = (...args) => args.reduce((acc, el)=>acc+=el, 0);
-  
-  console.log( summAllNumbers(1, 2, 3) ); // 6
-  console.log( summAllNumbers(1, 2, 3, 4) ); // 10
-  console.log( summAllNumbers(1, 2, 3, 4, 5) ); // 15
-  
-  /* 
+const summAllNumbers = (...args) => args.reduce((acc, el) => (acc += el), 0);
+
+console.log(summAllNumbers(1, 2, 3)); // 6
+console.log(summAllNumbers(1, 2, 3, 4)); // 10
+console.log(summAllNumbers(1, 2, 3, 4, 5)); // 15
+
+/* 
     Функция findEvery получает два аргумента - число и массив.
     Возвращает true если все элементы массива больше или равны числу.
     Иначе если есть хоть один элемент меньше числа, то возвращается false.
@@ -176,23 +176,17 @@
 //         return false;
 //       }
 //     }
-  
+
 //     return true;
 //   };
 
-  const findEvery = (num, arr) => arr.every(el=>el>=num)
-  
-  console.log( findEvery(5, [5, 6, 7, 8, 9]) ); // true
-  console.log( findEvery(6, [5, 6, 7, 8, 9]) ); // false
-  console.log( findEvery(4, [5, 6, 7, 8, 9]) ); // true
-  
+const findEvery = (num, arr) => arr.every(el => el >= num);
 
+console.log(findEvery(5, [5, 6, 7, 8, 9])); // true
+console.log(findEvery(6, [5, 6, 7, 8, 9])); // false
+console.log(findEvery(4, [5, 6, 7, 8, 9])); // true
 
-
-
-
-
-  /*
+/*
   Напишите функию getPropValues(arr, prop), принимающую 
   параметры arr - массив, и prop - имя ключа в объекте. 
   
@@ -209,16 +203,15 @@
 //   ];
 
 //   const getPropValues = (arr, prop) => arr.map(el=>el[prop]);
-  
+
 //   // Вызовы функции для проверки
 //   console.log( getPropValues(guests, "name") ); // ['Mango', 'Poly', 'Ajax', 'Chelsey']
-  
-//   console.log( getPropValues(guests, "age") ); // [20, 18, 30, 45]
-  
-//   console.log( getPropValues(guests, "isActive") ); // [true, false, true, false]
-  
 
-  /*      
+//   console.log( getPropValues(guests, "age") ); // [20, 18, 30, 45]
+
+//   console.log( getPropValues(guests, "isActive") ); // [true, false, true, false]
+
+/*      
   Напишите функцию setGuestState(guests, period), где
   guests - массив гостей, period - кол-во дней после
   которого считается что гость не активен.
@@ -233,38 +226,23 @@
 */
 
 const users = [
-    { name: 'Mango', inactiveDays: 15, isActive: true },
-    { name: 'Poly', inactiveDays: 8, isActive: false },
-    { name: 'Ajax', inactiveDays: 32, isActive: false },
-    { name: 'Chelsey', inactiveDays: 85, isActive: true }
-  ];
-  
-  const setGuestState = (guests, period) => guests.map(el=>({...el, isActive: el.inactiveDays<period}));
+  { name: "Mango", inactiveDays: 15, isActive: true },
+  { name: "Poly", inactiveDays: 8, isActive: false },
+  { name: "Ajax", inactiveDays: 32, isActive: false },
+  { name: "Chelsey", inactiveDays: 85, isActive: true }
+];
 
-  
-  // Вызовы функции для проверки
-  console.log(
-    setGuestState(users, 10)
-  ); // Объекты Mango, Ajax, Chelsey получат isActive false, а Poly наоборот true
-  
-  console.log(
-    setGuestState(users, 20)
-  ); // Объекты Ajax, Chelsey получат isActive false, а Mango и Poly наоборот true
-  
-  console.log(
-    setGuestState(users, 50)
-  ); // Объект Chelsey получит isActive false, а Mango, Poly и Ajax наоборот true
+const setGuestState = (guests, period) =>
+  guests.map(el => ({ ...el, isActive: el.inactiveDays < period }));
 
+// Вызовы функции для проверки
+console.log(setGuestState(users, 10)); // Объекты Mango, Ajax, Chelsey получат isActive false, а Poly наоборот true
 
+console.log(setGuestState(users, 20)); // Объекты Ajax, Chelsey получат isActive false, а Mango и Poly наоборот true
 
+console.log(setGuestState(users, 50)); // Объект Chelsey получит isActive false, а Mango, Poly и Ajax наоборот true
 
-
-
-
-
-
-
-  /*
+/*
   Напишите функию getActiveGuests(guests), принимающую 
   один параметр guests - массив объектов гостей. 
   
@@ -281,12 +259,9 @@ const users = [
 //     { name: "Chelsey", age: 45, isActive: false }
 //   ];
 //   const getActiveGuests = (guests) => guests.filter(el=>el.isActive)
-  
+
 //   // Вызовы функции для проверки
 //   console.log(getActiveGuests(guests)); // массив из 2-х объектов Mango и Ajax
-  
-
-
 
 /*      
   Напишите функцию getGuestsOlderThan(guests, age), где 
@@ -307,17 +282,15 @@ const users = [
 //   ];
 
 //   const getGuestsOlderThan = (guests, age) => guests.filter(el=>el.age>age)
-  
+
 //   // Вызовы функции для проверки
 //   console.log(getGuestsOlderThan(guests, 25)); // массив из 2-х объектов Ajax и Chelsey
-  
+
 //   console.log(getGuestsOlderThan(guests, 35)); // [{name: 'Chelsey', age: 45, isActive: false}]
-  
+
 //   console.log(getGuestsOlderThan(guests, 55)); // []
-  
 
-
-  /*
+/*
   Напишите функию getGuestById(guests, id), принимающую 
   guests - массив объектов гостей, id - идентификатор (число). 
   
@@ -332,27 +305,26 @@ const users = [
 //     { id: 3, name: 'Ajax', age: 30 },
 //     { id: 4, name: 'Chelsey', age: 45 }
 //   ];
-  
+
 //   const getGuestById = (guests, id) => guests.find(el=>el.id === id);
 
 //   // Вызовы функции для проверки
 //   console.log(
 //     getGuestById(guests, 3)
 //   ); // {id: 3, name: 'Ajax', age: 30}
-  
+
 //   console.log(
 //     getGuestById(guests, 1)
 //   ); // {id: 1, name: 'Mango', age: 20}
-  
+
 //   console.log(
 //     getGuestById(guests, 5)
 //   ); // undefined
 
-
 /*
   Используя метод reduce, посчитайте сумму 
   всех значений свойств объекта order.
-*/ 
+*/
 // const order = {
 //     bread: 10,
 //     apples: 25,
@@ -360,9 +332,8 @@ const users = [
 //     milk: 15,
 //     cheese: 40
 //   };
-  
-//   console.log(Object.keys(order).reduce((acc,el)=>acc+order[el],0)); // 150
 
+//   console.log(Object.keys(order).reduce((acc,el)=>acc+order[el],0)); // 150
 
 /*
   Напишите функцию getTotalPrice(products, order), где 
@@ -381,13 +352,13 @@ const users = [
 //     cheese: 30,
 //     chicken: 40
 //   };
-  
+
 //   const orderA = {
 //     bread: 2,
 //     apples: 4,
 //     chicken: 1
 //   };
-  
+
 //   const orderB = {
 //     bread: 1,
 //     milk: 2,
@@ -395,14 +366,13 @@ const users = [
 //   };
 
 //   const getTotalPrice = (products, order) => Object.keys(order).reduce((acc,el)=>acc+order[el]*products[el],0)
-  
+
 //   // Вызовы функции для проверки
 //   console.log(getTotalPrice(products, orderA)); // 140
-  
-//   console.log(getTotalPrice(products, orderB)); // 130
-  
 
-  /*     
+//   console.log(getTotalPrice(products, orderB)); // 130
+
+/*     
   Напишите функию allGuestsActive(guests), принимающую 
   один параметр guests - массив объектов гостей. 
   
@@ -417,17 +387,16 @@ const users = [
 //     { name: "Poly", isActive: false },
 //     { name: "Ajax", isActive: true }
 //   ];
-  
+
 //   const guestsB = [
 //     { name: "Mango", isActive: true },
 //     { name: "Poly", isActive: true },
 //     { name: "Ajax", isActive: true }
 //   ];
-  
+
 //  const allGuestsActive = (guests) => guests.some(el=>el.isActive===true);
 
 //   // Вызовы функции для проверки
 //   console.log(allGuestsActive(guestsA)); // false
-  
+
 //   console.log(allGuestsActive(guestsB)); // true
-  
