dad289de13bb645004c4da177f21eb5fd9137d45
Andrii <radomely@gmail.com>
2019-02-01 18:00:04 +0200

    added Task08

--- Task08/Task_08.01/js/scripts.js
+++ Task08/Task_08.01/js/scripts.js
@@ -4,9 +4,9 @@
   Есть кнопка с классом button, текст которой отображает 
   кол-во раз которое по ней кликнули, обновляется при каждом клике.
 */
-const  button = document.querySelector(".button");
-button.addEventListener("click",hendler);
+const button = document.querySelector(".button");
+button.addEventListener("click", hendler);
 let i = 0;
-function hendler (event){
+function hendler(event) {
   event.target.textContent = ++i;
 }
